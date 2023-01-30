42728eccc11a726b2daee543db4d1a7a165cf70b
Sergei_Kamendrovskii <se25051990@gmail.com>
2023-01-30 19:47:13 +0400

    Spring Learn commit

--- src/main/java/org/alishev/springcourse/MusicPlayer.java
+++ src/main/java/org/alishev/springcourse/MusicPlayer.java
@@ -2,8 +2,32 @@ package org.alishev.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {}
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
--- src/main/java/org/alishev/springcourse/TestSpring.java
+++ src/main/java/org/alishev/springcourse/TestSpring.java
@@ -10,6 +10,8 @@ public class TestSpring {
         //MusicPlayer musicPlayer = new MusicPlayer(musicBean);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
