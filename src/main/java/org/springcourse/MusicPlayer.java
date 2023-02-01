239df188fb9e29c799b54f3ac81b8f65a3052cea
Sergei_Kamendrovskii <se25051990@gmail.com>
2023-02-01 11:55:24 +0400

    Spring Learn commit - lesson 11 done (Qualifier)

--- src/main/java/org/springcourse/MusicPlayer.java
+++ src/main/java/org/springcourse/MusicPlayer.java
@@ -1,29 +1,24 @@
 package org.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component()
 public class MusicPlayer {
-
-    private ClassicMusic classicMusic;
-    private JazzMusic jazzMusic;
-    //private Music music;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicMusic classicMusic, JazzMusic jazzMusic) {
-        this.classicMusic = classicMusic;
-        this.jazzMusic = jazzMusic;
+    public MusicPlayer(@Qualifier("jazzMusic") Music music1,
+                       @Qualifier("classicMusic")Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
-
-
-    //public MusicPlayer(Music music) {
-    //    this.music = music;
-    //}
 
     public String playMusic() {
         //System.out.println("Playing: " + classicMusic.getSong());
         //System.out.println("Playing: " + jazzMusic.getSong());
-        return "Playing: " + jazzMusic.getSong();
+        return "Playing: " + music1.getSong() + " " + music2.getSong();
     }
 }
